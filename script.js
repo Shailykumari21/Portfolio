e0a9db8a34234175dbfef9f41ccd211fb52d6ac0
vansham-patel <priyanshupatel877@gmail.com>
2024-02-21 04:04:14 +0530

    updates

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
-
-
 
 function redirectToGithub() {
     window.location.href = 'https://github.com/vanshampatel';
